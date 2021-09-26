248d45b0271de92b28dc42f6df37cc9ca87af7d7
Robert-liu3 <robertliu80@gmail.com>
2021-09-26 15:24:49 -0400

    Initial commit

---
+++ src/Memory.java
@@ -0,0 +1,13 @@
+public class Memory {
+
+
+    public class StringInterval{
+        int id; // unique identifier for the string
+        int start; //index in memory holding the first character of the string
+        int length; //number of characters in the string
+        public void StringInter(int id, int start, int length) {
+
+        }
+
+    }
+}
